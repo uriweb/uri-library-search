155e6b8089eb741161e2805ca117edcaa6612da9
alexandragauss <alexandra_gauss@uri.edu>
2025-08-20 12:39:44 -0400

    Add js script

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,23 @@
 /**
  * SCRIPTS
  *
- * @package uri-library-search
+ * @package
  */
 
-( function() {
-
+( function( $ ) {
 	/* Do something on page load */
 
-})();
+	jQuery( '#primo-search input[name=query]' ).focus();
+	$( '#primo-search' ).on( 'submit', prepQuery );
+	function prepQuery() {
+		const query = $( 'input[name=query]', $( '#primo-search' ) ).val();
+		let q = 'https://uri-primo.hosted.exlibrisgroup.com/primo-explore/search?query=any,contains,';
+		q += query;
+		q += '&tab=default_tab&search_scope=Books_More&vid=01URI&offset=0&fn=search';
+		if ( query ) {
+			window.location.href = q;
+		}
+		return false;
+	}
+}( jQuery ) );
+
