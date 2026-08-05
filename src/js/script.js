d4176f6035b8629afcccdc08785df816fbe3dbe0
alexandragauss <alexandra_gauss@uri.edu>
2026-08-05 15:09:48 -0400

    Encode query input

--- src/js/script.js
+++ src/js/script.js
@@ -12,7 +12,7 @@
 	function prepQuery() {
 		const query = $( 'input[name=query]', $( '#library-primo-search' ) ).val();
 		let q = 'https://uri.primo.exlibrisgroup.com/nde/search?query=';
-		q += query;
+		q += encodeURIComponent( query );
 		q += '&tab=Everything&search_scope=MyInst_and_CI&vid=01URI_INST:01URI_INST_NDE&lang=en';
 		if ( query ) {
 			window.location.href = q;
